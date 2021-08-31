c83e8df6bd6247452b9d170f926aba100bf7bd40
Christopher Boyle <cboyle824@gmail.com>
2021-08-31 10:07:37 -0400

    Built index.js, server.js, and starting to build the posts router. get requests can be made to the endpoint successfully

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const Posts = require("./posts-model");
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  const posts = await Posts.find(req.query);
+  try {
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({
+      message: "The posts information could not be retrieved",
+    });
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+
+const server = express();
+
+const postsRouter = require("./posts/posts-router");
+
+server.use(express.json());
+server.use("/api/posts", postsRouter);
+
+server.use("*", (req, res) => {
+  res.status(404).send(`
+  <p>Not Found</p>
+  `);
+});
+
+module.exports = server;

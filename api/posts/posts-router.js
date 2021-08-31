27ad745a8889505f83fd96492872f02d1039a675
Christopher Boyle <cboyle824@gmail.com>
2021-08-31 12:42:35 -0400

    DELETE passes all tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -73,4 +73,23 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const post = await Posts.findById(id);
+  try {
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      await Posts.remove(id);
+      res.status(200).json(post);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post could not be removed",
+    });
+  }
+});
+
 module.exports = router;
